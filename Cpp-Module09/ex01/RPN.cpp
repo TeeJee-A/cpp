ed37f1ced66dfa855b47cb675b14694799c949af
Ayoub Taji <ataji@e1r11p4.1337.ma>
2023-03-31 02:01:40 +0000

    fix some issues

--- Cpp-Module09/ex01/RPN.cpp
+++ Cpp-Module09/ex01/RPN.cpp
@@ -2,58 +2,66 @@
 
 std::stack<int> stack;
 
-bool is_operator(char *operations) {
-    if (!strcmp(operations, "+") || !strcmp(operations, "-") || !strcmp(operations, "*") || !strcmp(operations, "/"))
+bool is_operator(char operations) {
+    if (operations == '+' || operations == '-' || operations == '/' || operations == '*')
         return true;
     return false;
 }
 
-bool parse_operations(char *operations) {
+bool parse_operations(char operations) {
     if (is_operator(operations))
         return true;
-    else {
-        size_t i = -1;
-        while (operations[++i]) {
-            if (isdigit(operations[i]))
-                return true;
-        }
-    }
+    else if (isdigit(operations))
+        return true;
     return false;
 }
 
-void execute(char *operations) {
+void execute(char operations) {
     int result = 0;
-    int oper = atoi(operations);
+    int oper = atoi(&operations);
     if (stack.size() > 1 && is_operator(operations)) {
         int one = stack.top();
         stack.pop();
         int two = stack.top();
         stack.pop();
-        if (operations[0] == '+')
+        if (operations == '+')
             result = two + one;
-        else if (operations[0] == '-')
+        else if (operations == '-')
             result = two - one;
-        else if (operations[0] == '*')
+        else if (operations == '*')
             result = two * one;
-        else if (operations[0] == '/')
+        else if (operations == '/') {
+            if (one == 0) {
+                std::cout << "Error" << std::endl;
+                exit(1);
+            }
             result = two / one;
+        }
         stack.push(result);
-    } else if (!is_operator(operations)){
+    } else if (!is_operator(operations)) {
         stack.push(oper);
+    } else if (stack.size() == 1 && is_operator(operations)) {
+        std::cout << "Error" << std::endl;
+        exit(1);
     }
 }
 
-void begin(char *operations) {
-    char *token;
-    token = strtok(operations, " ");
-    while (token != NULL) {
-        if (!parse_operations(token)) {
-            std::cout << "Error" << std::endl;
-            return ;
+void begin(char* operations) {
+    int i = 0;
+    while (operations && operations[i]) {
+        if (operations[i] != ' ') {
+            if (!parse_operations(operations[i])) {
+                std::cout << "Error" << std::endl;
+                exit(1);
+            }
+            execute(operations[i]);
         }
-        execute(token);
-        token = strtok(NULL, " ");
+        i++;
     }
     if (stack.size() == 1)
         std::cout << stack.top() << std::endl;
+    else {
+        std::cout << "Error" << std::endl;
+        exit(1);
+    }
 }
--- Cpp-Module09/ex01/main.cpp
+++ Cpp-Module09/ex01/main.cpp
@@ -1,8 +1,15 @@
 #include "RPN.hpp"
 
 int main(int ac, char **av) {
-    if (ac == 2)
+    if (ac == 2) {
+        if (strlen(av[1]) == 0) {
+            std::cout << "Error" << std::endl;
+            return 1;
+        }
         begin(av[1]);
-    else 
+    }
+    else {
         std::cout << "Error" << std::endl;
+        return 1;
+    }
 }
